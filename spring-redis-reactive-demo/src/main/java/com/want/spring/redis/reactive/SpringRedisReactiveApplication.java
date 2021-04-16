3f69654a6dc974c3dd546f341bdb8389cf48a9e8
WanT0011 <2374909509@qq.com>
2021-04-16 22:58:56 +0800

    增加输出提示

--- spring-redis-reactive-demo/src/main/java/com/want/spring/redis/reactive/SpringRedisReactiveApplication.java
+++ spring-redis-reactive-demo/src/main/java/com/want/spring/redis/reactive/SpringRedisReactiveApplication.java
@@ -1,5 +1,6 @@
 package com.want.spring.redis.reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author want
  * @createTime 2021.04.12.21:54
  */
+@Slf4j
 @SpringBootApplication
 public class SpringRedisReactiveApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringRedisReactiveApplication.class,args);
+        log.info("访问：http://localhost:8080/swagger-ui/index.html");
     }
 }
